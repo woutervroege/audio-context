c776e49bab3f3c0f4286f6b4f7497c765734bb38
Wouter Vroege <wouter@woutervroege.nl>
2020-11-08 14:15:31 +0100

    added proper description and index file
    Signed-off-by: Wouter Vroege <wouter@woutervroege.nl>

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './audio-buffer-source-node.js';
+import './audio-buffer.js';
+import './audio-context.js';
+import './audio-destination-node.js';
+import './biquad-filter-node.js';
+import './convolver-node.js';
+import './delay-node.js';
+import './dynamics-compressor-node.js';
+import './gain-node.js';
